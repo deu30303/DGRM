5a4509759b4379d31817dcdf98a4ef0933235d8a
Sungwon Park <39057852+deu30303@users.noreply.github.com>
2024-08-07 11:50:27 +0900

    Update detection_utils.py

--- detection_utils.py
+++ detection_utils.py
@@ -16,85 +16,46 @@ class GradReverse(torch.autograd.Function):
         
 def grad_reverse(x, lambd=1.0, reverse=True):
     return GradReverse.apply(x, lambd, reverse)
+
+def mask_tokens(inputs, tokenizer, mlm_probability=0.15, pad=True):
+    labels = inputs.clone()
+    
+    probability_matrix = torch.full(labels.shape, mlm_probability)
+    special_tokens_mask = [
+       tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+    ]
+    probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+    if tokenizer._pad_token is not None:
+        padding_mask = labels.eq(tokenizer.pad_token_id)
+        probability_matrix.masked_fill_(padding_mask, value=0.0)
+    
+    masked_indices = torch.bernoulli(probability_matrix).bool()
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.15)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+    return inputs
   
     
-def random_seq(src):
-    #adding [SEP] to unify the format of samples for NSP
-    batch_size = src.size(0)
-    length = src.size(1)
-    dst = []
-    for i in range(batch_size):
-        cur = src[i]
-        try:
-            first_pad = (cur.tolist()).index(0)
-        except:
-            first_pad = 256
-        cur = cur[1:first_pad].tolist()
-        cur = random_string(cur)
-        padding = [0] * (length - len(cur))
-        dst.append(torch.tensor([101] + cur + padding))
-    return torch.stack(dst)
+class InfoNCE(): 
+    def __init__(self,temperature):
+        self.temperature = temperature
 
- 
-def random_string(str):
-    #randomly split positive samples into two halves and add [SEP] between them
-    try:
-        str.remove(102)
-    except:
-        pass
-    try:
-        str.remove(102)
-    except:
-        pass
-    len1 = len(str)
-    if len1 == 1:
-        cut = 1
-        str = str[:cut] + [102] + str[cut:] + [102]
-    elif len1 == 0:
-        pass
-    else:
-        cut = np.random.randint(1, len1)
-        str = str[:cut] + [102] + str[cut:] + [102]
-    return str
-
-def change_string(str):
-    #creating negative samples for NSP by randomly splitting positive samples
-    #and swapping two halves
-    try:
-        str.remove(102)
-    except:
-        pass
-    try:
-        str.remove(102)
-    except:
-        pass
-    len1 = len(str)
-    if len1 == 1:
-        cut = 1
-        str = str[:cut] + [102] + str[cut:] + [102]
-    elif len1 == 0:
-        pass
-    else:
-        cut = np.random.randint(1, len1)
-        str = str[:cut] + [102] + str[cut:] + [102]
-    return str
-
-def get_permutation_batch(src, src_mask):
-    #create negative samples for Next Sentence Prediction
-    batch_size = src.size(0)
-    length = src.size(1)
-    dst = []
-    dst_mask = []
-    lbl = []
-    for i in range(batch_size):
-        cur = src[i]
-        mask = src_mask[i].tolist() + [0]
-        first_pad = (cur.tolist() + [0]).index(0)
-        cur = cur[1:first_pad].tolist()
-        cur = change_string(cur)
-        lbl.append(1)
-        padding = [0] * (length - len(cur) - 1)
-        dst.append(torch.tensor([101] + cur + padding))
-        dst_mask.append(torch.tensor(mask))
         
-    return torch.stack(dst), torch.stack(dst_mask), torch.tensor(lbl)
+    def __call__(self,out): 
+        out = F.normalize(out,dim=-1)
+        bs = int(out.shape[0]/2)
+   
+        out_1,out_2 =  out.split(bs,dim=0) # (B,D) , (B,D) 
+
+        sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / self.temperature)
+        mask = (torch.ones_like(sim_matrix) - torch.eye(2 * bs, device=sim_matrix.device)).bool()
+        sim_matrix = sim_matrix.masked_select(mask).view(2 * bs, -1)
+        
+        pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / self.temperature)
+
+        pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+        loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+        
+        return loss       
